6c829783c2482def780619a3951516648d844f92
User <toma2006.06.11.@gmail.com>
2024-10-31 17:11:25 +0400

    Создано окно и виджеты

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,23 @@
 from tkinter import*
+from PIL import Image,ImageTk
+import requests
+from io import BytesIO # позволяет работать с двоичными (1,0) байтами,т.к картинка из инета будет приходить в виде набора байтов, ее(картинку) надо превратить в изображение
+
+window = Tk()# создаем окно
+window.title("Cats!")# заголовок
+window.geometry("600x480")
+
+label = Label() # метка
+label.pack()
+
+url = "https: // cataas.com/cat" # адрес интернете
+img = load_image(url)
+
+
+if img: # если картинка не путая
+    label.config(image=img)
+    label.image = img # картинка происвоина и сборщик мусора ее не удалит
+
+
+
+window.mainloop()
